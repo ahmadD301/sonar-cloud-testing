f3763cf3668f5694aee82789bcd489ee4f1b0ac4
osama&asem <asemhesham55@gmail.com>
2024-04-01 05:17:13 +0300

    update 5.5

--- JS-Files/venue.js
+++ JS-Files/venue.js
@@ -212,24 +212,7 @@ class VenuePage extends Page {
     return tempMap;
   }
 
-  compareTimes(time1, time2) {
-    const [hours1, minutes1] = time1.split(":").map(Number);
-    const [hours2, minutes2] = time2.split(":").map(Number);
 
-    if (hours1 < hours2) {
-      return -1;
-    } else if (hours1 > hours2) {
-      return 1;
-    } else {
-      if (minutes1 < minutes2) {
-        return -1;
-      } else if (minutes1 > minutes2) {
-        return 1;
-      } else {
-        return 0;
-      }
-    }
-  }
   isBooked(id, startTime, endTime, startDate, endDate) {
     let tempMap = DB.reservationMap;
     let date1 = startDate;
